cb7a4037de0e362d7ed4ba6b7ca176b3ddb84353
Evgeny Zhukov <ezvgeniy0011@gmail.com>
2021-05-10 17:34:02 +0300

    2

--- Perceptron.cpp
+++ Perceptron.cpp
@@ -6,7 +6,7 @@ using std::endl;
 class Neuron
 {
 public:
-	Neuron(int weight,int input)
+	Neuron(int weight, int input)
 	{
 		this->weight = weight;
 		this->input = input;
@@ -25,6 +25,10 @@ public:
 	{
 		return this->weight;
 	}
+	int setWeight(int weight)
+	{
+		return(this->weight = weight);
+	}
 private:
 	int input;
 	int weight;
@@ -42,124 +46,80 @@ int Output(T a,T b)
 		return 1;
 	}
 }
+
+template<typename T>
+int Output(T a, T b,T c)
+{
+	if (a.Output(a.getInput(), a.getWeight()) + b.Output(b.getInput(), b.getWeight()) + c.Output(c.getInput(), c.getWeight()) <= 0)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+template<typename T>
+int Check(T a,T b, T c, int ref)
+{
+	if (Output(a, b, c) < ref)
+	{
+		return 1;
+	}
+	else if (Output(a, b, c) > ref)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+}
 int main()
 {
-	Neuron S1_A1(-1,1);
-	Neuron S2_A1(1, 1);
+	int i = 1;
+	int j = 1;
+	const int ref = 1;
+
+	Neuron S1_A1(-1,i);
+	Neuron S2_A1(1, j);
 	cout<<Output(S1_A1, S2_A1)<<endl;
 	
+	Neuron S1_A2(1, i);
+	Neuron S2_A2(-1, j);
 
-	Neuron S1_A2(1, 1);
-	Neuron S2_A2(1, 1);
-	Neuron S1_A3(-1, 1);
-	Neuron S2_A3(1, 1);
+	cout << Output(S1_A2, S2_A2) << endl;
+	Neuron S1_A3(1, i);
+	Neuron S2_A3(1, j);
+
+	cout << Output(S1_A3, S2_A3) << endl;
+
+	
+
+	Neuron A1_R1(0, Output(S1_A1, S2_A1));
+	Neuron A2_R1(0, Output(S1_A2, S2_A2));
+	Neuron A3_R1(0, Output(S1_A2, S2_A2));
+
+	cout <<"Output R = "<< Output(A1_R1, A2_R1, A3_R1) << endl;
+	cout << "ref = " << ref << endl;
+
+	int check_value;
+	check_value = Check(A1_R1, A2_R1, A3_R1, ref);
+
+	for (int i = 0; i < 100; i++)
+	{
+		A1_R1.setWeight(A1_R1.getWeight() + check_value * Output(S1_A1, S2_A1));
+		A2_R1.setWeight(A2_R1.getWeight() + check_value * Output(S1_A2, S2_A2));
+		A3_R1.setWeight(A3_R1.getWeight() + check_value * Output(S1_A3, S2_A3));
+		check_value = Check(A1_R1, A2_R1, A3_R1, ref);
+		
+		if (Check(A1_R1, A2_R1, A3_R1, ref) == 0)
+		{
+			cout << "Output R = " << Output(A1_R1, A2_R1, A3_R1) << endl;
+			cout << "ref = " << ref << endl;
+			cout<<"i = "<<i << endl;
+			break;
+		}
+	}
 	return 0;
 }
-
-//const int n = 2;
-//
-//int x[n] = { 1,1 };
-//int w_1[3] = { -1,1,1 };
-//int w_2[3] = { 1,-1,1 };
-//int w_3[3] = { 0,0,0 };
-//int s_1, s_2, s_3, a;
-//int ref = 1;
-//int reac;
-//for (int i = 0; i < 1; i++)
-//{
-//	s_1 = x[0] * w_1[i] + x[1] * w_2[i];
-//	if (s_1 <= 0)
-//	{
-//		s_1 = 0;
-//	}
-//	else
-//	{
-//		s_1 = 1;
-//	}
-//	s_2 = x[0] * w_1[i + 1] + x[1] * w_2[i + 1];
-//	if (s_2 <= 0)
-//	{
-//		s_2 = 0;
-//	}
-//	else
-//	{
-//		s_2 = 1;
-//	}
-//	s_3 = x[0] * w_1[i + 2] + x[1] * w_2[i + 2];
-//	if (s_3 <= 0)
-//	{
-//		s_3 = 0;
-//	}
-//	else
-//	{
-//		s_3 = 1;
-//	}
-//	cout << s_1 << endl << s_2 << endl << s_3 << endl;
-//}
-//
-//a = s_1 * w_3[0] + s_2 * w_3[1] + s_3 * w_3[2];
-//if (a <= 0)
-//{
-//	a = 0;
-//}
-//else
-//{
-//	a = 1;
-//}
-//cout << a << endl;
-//
-//if (a == ref)
-//{
-//	a = 0;
-//}
-//else if (a < ref)
-//{
-//	a = 1;
-//}
-//else
-//{
-//	a = -1;
-//}
-//for (int i = 0; i < 1; i++)
-//{
-//	s_1 = w_3[0] + a * s_1;
-//	if (s_1 <= 0)
-//	{
-//		s_1 = 0;
-//	}
-//	else
-//	{
-//		s_1 = 1;
-//	}
-//	s_2 = w_3[1] + a * s_2;
-//	if (s_2 <= 0)
-//	{
-//		s_2 = 0;
-//	}
-//	else
-//	{
-//		s_2 = 1;
-//	}
-//	s_3 = w_3[2] + a * s_3;
-//	if (s_3 <= 0)
-//	{
-//		s_3 = 0;
-//	}
-//	else
-//	{
-//		s_3 = 1;
-//	}
-//	cout << s_1 << endl << s_2 << endl << s_3 << endl;
-//
-//}
-//
-//a = s_1 * w_3[0] + s_2 * w_3[1] + s_3 * w_3[2];
-//if (a <= 0)
-//{
-//	a = 0;
-//}
-//else
-//{
-//	a = 1;
-//}
-//cout << a << endl;
