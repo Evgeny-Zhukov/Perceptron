e81cdc0e240d67abb4df6d1ba143041a4fc9b4fa
Evgeny Zhukov <ezvgeniy0011@gmail.com>
2021-05-15 11:13:28 +0300

    4

--- Perceptron.cpp
+++ Perceptron.cpp
@@ -1,168 +1,85 @@
 ﻿#include <iostream>
-
 using std::cout;
 using std::endl;
 
 class Neuron
 {
 public:
-	int input;
-	int weight;
-	int output;
+	
+
 	Neuron()
 	{
-		this->output = 0;
-		this->weight = 1;
-	}
-	Neuron(int weight, int output)
-	{
-		this->weight = weight;
-		this->output = output;
-		
-	}
 
-	int Out(int weight, int input)
-	{
-		return (this->output = (input * weight));
 	}
-	
-	
-	/*int getInput()
+	Neuron(int input,int weight)
 	{
-		return this->input;
+		this->input = input;
+		this->weight = weight;
+		this->output = input;
 	}
-	int getWeight()
-	{
-		return this->weight;
-	}
-	
-	int getOutput()
-	{
-		return output;
-	}*/
-	
-	int Inp(Neuron a,Neuron b)
-	{
-		if ((a.output*a.weight+b.output*b.weight)<=0)
-		{
-			return (this->input=0);
-		}
-		else
-		{
-			return (this->input=1);
-		}
-	}
-	int Inp(Neuron a, Neuron b, Neuron c)
-	{
-		if ((a.output * a.weight + b.output * b.weight + c.output*c.weight)<=0)
-		{
-			return (this->input = 0);
-		}
-		else
-		{
-			return (this->input = 1);
-		}
-	}
-	
 	void Print()
 	{
-		cout << "input " << input << " weight " << weight << " output " << output << endl;
+		cout << "input = "<<input << " weight = " << weight << " output = " << output << endl;
+	}
+	void setInput(Neuron a,Neuron b)
+	{
+		input = (a.output*a.weight+b.output*b.weight);
+	}
+	void setInput(Neuron a, Neuron b,Neuron c)
+	{
+		input = (a.output * a.weight + b.output * b.weight+c.output*c.weight);
+	}
+	void Output()
+	{
+		if (input < 0)
+		{
+			output = 0;
+		}
+		else
+		{
+			output = 1;
+		}
 	}
 	
 private:
-	
+	int input;
+	int weight;
+	int output;
+
 };
-int Check(int a, int ref)// сравнивает выходной сигнал R нейрона с эталоном
-{
-	if (a < ref)
-	{
-		
-		return 1;
-	}
-	else if (a > ref)
-	{
-		
-		return -1;
-	}
-	else
-	{
-		
-		return 0;
-	}
-}
+
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-
 	int i = 1;
 	int j = 1;
+	Neuron S1_A1(i,-1);
+	S1_A1.Print();
+	Neuron S2_A1(j,1);
+	S2_A1.Print();
+	Neuron S1_A2(i,1);
+	S1_A2.Print();
+	Neuron S2_A2(j,-1);
+	S2_A2.Print();
+	Neuron S1_A3(i,1);
+	S1_A3.Print();
+	Neuron S2_A3(j,1);
+	S2_A2.Print();
 
-	const int ref = -1;
+	Neuron A1;
+	A1.setInput(S1_A1, S2_A1);
+	A1.Output();
+	A1.Print();
 
-	Neuron S1_A1(-1,i);
-	Neuron S2_A1(1, j);
-	Neuron S1_A2(1, i);
-	Neuron S2_A2(-1, j);
-	Neuron S1_A3(1, i);
-	Neuron S2_A3(1, j);
+	Neuron A2;
+	A2.setInput(S1_A2, S2_A2);
+	A2.Output();
+	A2.Print();
 
-	//определяем вес от S к А
-	Neuron A1_R1;
-	A1_R1.Inp(S1_A1, S2_A1);
-	A1_R1.Out(A1_R1.weight,A1_R1.Inp(S1_A1, S2_A1));
-	A1_R1.Print();
+	Neuron A3;
+	A3.setInput(S1_A3, S2_A3);
+	A3.Output();
+	A3.Print();
 
-	Neuron A2_R1;
-	A2_R1.Inp(S1_A2, S2_A2);
-	A2_R1.Out(A2_R1.weight, A2_R1.Inp(S1_A2, S2_A2));
-	A2_R1.Print();
 
-	Neuron A3_R1;
-	A3_R1.Inp(S1_A3, S2_A3);
-	A3_R1.Out(A3_R1.weight, A3_R1.Inp(S1_A3, S2_A3));
-	A3_R1.Print();
-	
-	Neuron R;
-	R.Out(R.weight, R.Inp(A1_R1, A2_R1, A3_R1));
-	R.Print();
-
-	
-	for (int k = 0; k < 10; k++)
-	{
-		if (Check(R.output, ref) == 1)
-		{
-			cout << "weight+" << endl;
-			A1_R1.weight = (A1_R1.weight + Check(R.output, ref) * A1_R1.output);
-			A2_R1.weight = (A2_R1.weight + Check(R.output, ref) * A2_R1.output);
-			A3_R1.weight = (A3_R1.weight + Check(R.output, ref) * A3_R1.output);
-			R.Out(R.weight, R.Inp(A1_R1, A2_R1, A3_R1));
-		}
-		else if (Check(R.output, ref) == -1)
-		{
-			cout << "weight-" << endl;
-			A1_R1.weight = (A1_R1.weight + Check(R.output, ref) * A1_R1.output);
-			A2_R1.weight = (A2_R1.weight + Check(R.output, ref) * A2_R1.output);
-			A3_R1.weight = (A3_R1.weight + Check(R.output, ref) * A3_R1.output);
-			R.Out(R.weight, R.Inp(A1_R1, A2_R1, A3_R1));
-		}
-		else
-		{
-			cout << "weight unchanged" << endl;
-			A1_R1.weight = (A1_R1.weight + Check(R.output, ref) * A1_R1.output);
-			A2_R1.weight = (A2_R1.weight + Check(R.output, ref) * A2_R1.output);
-			A3_R1.weight = (A3_R1.weight + Check(R.output, ref) * A3_R1.output);
-			R.Out(R.weight, R.Inp(A1_R1, A2_R1, A3_R1));
-		}
-		cout << "k = " << k << endl;
-		A1_R1.Print();
-		A2_R1.Print();
-		A3_R1.Print();
-		R.Print();
-		if (R.output == ref)
-		{
-			break;
-		}
-	}
-	
 	return 0;
 }
